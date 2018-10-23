bdacf4e62a7101cb895714e72cea6f4c9a5ca7c7
gogoing1 <cq_816102@163.com>
2018-10-23 12:13:09 +0800

    Initial commit

---
+++ lib/firstView.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FirstApp());
+}
+
+class FirstApp extends StatelessWidget {
+
+  //根布局
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Fade Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyFadeTest(title: 'Fade Demo'),
+    );
+  }
+}
+
+class MyFadeTest extends StatefulWidget {
+
+  MyFadeTest({Key key, this.title}) :super(key: key);
+
+  String title;
+
+  @override
+  _MyFadeTest createState() => new _MyFadeTest();
+}
+
+class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin {
+
+  AnimationController controller;
+  CurvedAnimation curve;
+
+  @override
+  void initState() {
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    curve = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new FadeTransition(
+          opacity: curve,
+          child: new FlutterLogo(
+            size: 100.0,
+          ),
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Fade',
+        child: new Icon(Icons.brush),
+        onPressed: () {
+          controller.forward();
+        },
+      ),
+    );
+  }
+}
